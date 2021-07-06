3796d5c6c4b6d150115b5e4f2aab4da82ba2b888
Jean-Baptiste-CHENEAU <87029254+Jean-Baptiste-CHENEAU@users.noreply.github.com>
2021-07-06 08:48:28 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Ajout d'un offset pour éviter que la Nav n'overlay les titres lors de l'utilisation du Spyscroll
+
+$('.nav-link').on('click',function() {
+    $('.navbar-collapse').collapse('hide');
+  });
